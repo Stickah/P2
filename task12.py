0a8f2536e68222830f71540b056a5b787b30b177
Vladislav <vlad-taranin@yandex.ru>
2023-02-11 15:24:07 +0300

    new

--- task12.py
+++ task12.py
@@ -1,6 +1,5 @@
-x = int(input())
-y = int(input())
-for i in range(x):
-    for j in range(y):
-        if x == i + j and y == i * j:
-            print(i, j)
+first_number, second_number = int(input("Сумма чисел: ")), int(input("Произведение чисел: "))
+for i in range(1, first_number+1):
+    for j in range(1, first_number+1):
+        if i + j == first_number and i * j == second_number:
+            print(f"Задуманные Петей Числа {first_number} {second_number} -> {i} {j}")
